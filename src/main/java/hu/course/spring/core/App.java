5b190f3a90f77cbf327ff1333aeea145e9cde49b
Balázs Gábor <mail.gabor.balazs@gmail.com>
2018-05-14 20:06:58 +0200

    Starting DI

--- src/main/java/hu/course/spring/core/App.java
+++ src/main/java/hu/course/spring/core/App.java
@@ -1,5 +1,9 @@
 package hu.course.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import hu.course.spring.core.beans.Client;
 import hu.course.spring.core.loggers.ConsoleEventLogger;
 
@@ -15,11 +19,35 @@ public class App {
 		app.eventLogger = new ConsoleEventLogger();
 
 		app.logEvent("Some event for user 1");
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+
+		app = (App) ctx.getBean("app");
+
+		app.logEvent("Some event for user 2");
+		
+		((ConfigurableApplicationContext)ctx).close();
 	}
 
 	public void logEvent(String msg) {
 		String message = msg.replace(client.getId(), client.getFullName());
 		eventLogger.logEvent(message);
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public ConsoleEventLogger getEventLogger() {
+		return eventLogger;
+	}
+
+	public void setEventLogger(ConsoleEventLogger eventLogger) {
+		this.eventLogger = eventLogger;
 	}
 
 }
